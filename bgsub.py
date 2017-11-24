d3e3d1aeca5e5d61ed9acc87bbbd024ace59ab75
MohamedAli1995 <mohamedali166@gmail.com>
2017-11-24 20:34:59 +0200

    hand region size detected

--- bgsub.py
+++ bgsub.py
@@ -1,11 +1,61 @@
 import numpy as np
 import cv2
+import time;
+hand_region_Window=[60,60,70,100]
+wristx=0
+wristy=0
+faceExisted_once=False;
+a=0
+b=0
+c=0
+d=0
+
+
+frame_max_x=0;
+frame_max_y=0;
+def updateHandRect(frame,maxLoc):
+    [x,y]=maxLoc
+    [tempx,tempy]=maxLoc
+    [tempx_right, tempy_bottom] = maxLoc
+    while(np.sum(np.sum(frame[tempy-50:tempy+50,tempx-5:tempx+5]))!=0):
+        tempx-=5
+    while (np.sum(np.sum(frame[tempy - 50:tempy + 50, tempx_right -5:tempx_right +5]))!= 0 and tempx_right+5<frame_max_x):
+        tempx_right += 5
+
+    while (np.sum(np.sum(frame[tempy-5:tempy+5 , x-50:x+50])) != 0):
+        tempy -= 5
+
+    tempy_bottom=tempy+200 # TODO: make height as a ratio of width
+    #if tempy or tempx falls below 0
+    tempy=np.maximum(0,tempy)
+    tempy=np.minimum(frame_max_y-1,tempy)
+
+    tempy_bottom = np.maximum(0, tempy_bottom)
+    tempy_bottom = np.minimum(frame_max_y - 1, tempy_bottom)
+
+    tempx=np.maximum(0,tempx)
+    tempx = np.minimum(frame_max_x - 1, tempx)
+
+    tempx_right=np.maximum(0,tempx_right)
+    tempx_right = np.minimum(frame_max_x - 1, tempx_right)
+
+    #print(" top left : (x1,y1)(x2,y2)  (",tempx," , ",tempy," ) (",tempx_right," , ",tempy_bottom," ) ")
+    return [tempx,tempy,tempx_right,tempy_bottom]
+
+
+def getWristPoints(frame):
+    print(frame.shape)
+    cv2.imshow  ("Sdsa",frame)
+    return [5,5]
+start_time=time.time() #used to call functions every x seconds
+forget_frames=30;
+
+call_time=0.5
 faceExist = False;  # to figure out wether at least face exist or no
 faceWasDetectedBefore=False;
 
 face_cascade = cv2.CascadeClassifier('front_face.xml')
 def detectSingleFace(frame):
-    print(frame)
     # haar cascade face detection
     faces = face_cascade.detectMultiScale(frame, 1.3, 5) #using haar cascade classifier to detect faces in image
     if len(faces)!=1:
@@ -133,18 +183,33 @@ x=0
 y=0
 w=0
 h=0
+
 while(1):
     ret, frame = cap.read()
+    [frame_max_y,frame_max_x,dummy]=frame.shape
     frame = cv2.flip(frame, 1)#flip image to adjust it
+    ######################
+    ########## used to call face detection every x seconds only
+    if faceWasDetectedBefore==False:
+        faceExist,face=detectSingleFace(frame)
 
-    faceExist,face=detectSingleFace(frame);
+    elif (time.time()-start_time>call_time):
+        faceExist, face = detectSingleFace(frame)
+        start_time=time.time()
+    ############################################################
+    ############################################################
+
+
+
     if faceExist:
         [x, y, w, h] = face
-        print(" ( ",x," , ",y," , ", w , " , ",h," ) ")
+        faceExisted_once=True; # to know that face existed once in the image then  we should track it
+        #print(" ( ",x," , ",y," , ", w , " , ",h," ) ")
         if faceWasDetectedBefore==False:
             faceWasDetectedBefore=True;
 
     if faceWasDetectedBefore:
+
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
         frame[y:y + w, x:x + h] = [0, 0, 0];
 
@@ -160,9 +225,14 @@ while(1):
     normalized_skin=np.uint8(np.double(skin_ycrcb)/255.0)#normalizing  skin_ycrbcb
     normalized_skin_3D=cv2.cvtColor(normalized_skin, cv2.COLOR_GRAY2RGB); #convert greyscale normalized skin_ycrbcb to rgb  to be multiplied by frame (rgb)
     fgmask_on_skin=np.uint8(fgmask*normalized_skin) # skin color of moving parts * original frame
+
+    kernel_erosion = np.ones((3, 3), np.uint8)
+    fgmask_on_skin = cv2.erode(fgmask_on_skin, kernel_erosion, iterations=1)
+
     fgmask_on_skin_normalized=np.uint8(np.double(fgmask_on_skin)/255.0) #normalized skin color of moving parts * original frame
     fgmask_on_skin_3D=np.uint8(np.double(cv2.cvtColor(fgmask_on_skin, cv2.COLOR_GRAY2RGB))/255.0);
     rgb_masked_image=fgmask_on_skin_3D*frame
+
     edged_fgbmask = cv2.Canny(normalized_skin_3D*frame, 35, 125)*fgmask_on_skin_normalized # applying canny edge detection
 
     kernel = np.ones((100, 100), np.double) / (100*100) #filter to calculate edges within a box
@@ -170,33 +240,47 @@ while(1):
     edges_per_area_image = cv2.filter2D(np.double(edged_fgbmask), -1, kernel)# edges per area ( fixed area)
     minVal, maxVal, minLoc, maxLoc=cv2.minMaxLoc(edges_per_area_image) #getting maximum point of maximum edges per area value
     #print("min val : "  ,minVal," max val ",maxVal," min Loc ",minLoc," max Loc ",maxLoc)
+
     tempx1,tempy1=maxLoc
 
     # yield the current window
 
-    if faceExist: # if face exist then draw box over point of maximum edges per area
+    if faceExisted_once: # if face exist then draw box over point of maximum edges per area
         #print("fab..")
-        if maxVal>10.0:
-            to_detect_hands = True;
-            tempx1_new, tempy1_new=tempx1,tempy1
+        if maxVal>3 and forget_frames <=0: # forget_frames used to wait until stablization
+            pixel_summation = 0;
+            #[a,b,c,d]=hand_region_Window
+            pixels_summation = np.sum(np.sum(fgmask_on_skin[tempy1 - b:tempy1 +d,tempx1 - a:tempx1 + c]))
+            if(pixels_summation>100 or True ):#not working ##4 is threshold for summing white pixels of moving object as 2nd feature
+                to_detect_hands = True;
+                [a,b,c,d]=updateHandRect(fgmask_on_skin,maxLoc)
+                tempx1_new, tempy1_new = a,b
+
+            #print("pixels summation : ", pixels_summation/10000," max value ",maxVal)
+        else:
+            if(forget_frames>-10):
+                forget_frames-=1
+            #print("max value ",maxVal)
     if to_detect_hands:
-        cv2.rectangle(frame, (tempx1_new - 60, tempy1_new - 60), (tempx1_new + 70, tempy1_new + 100), [255, 0, 0], 3)
-    else:
-       print("face error ")
-   # cv2.imshow('fgmask x normalized skin', fgmask_on_skin)
+        #[a,b,c,d]=hand_region_Window
+        #[wristx, wristy]=getWristPoints(frame[tempy1_new - b:tempy1_new +d,tempx1_new - a:tempx1_new + c])
+        print(" top left : (x1,y1)(x2,y2)  (", a, " , ", b, " ) (",c, " , ",d, " ) ")
+        cv2.rectangle(frame, (a,b), (c,d), [0, 1, 0.5], 3)
+
+    cv2.imshow('fgmask x normalized skin', fgmask_on_skin)
     cv2.imshow('original Frame', frame)
-    #cv2.imshow('fgmask',fgmask)
-    #cv2.imshow('fgmask colored',rgb_masked_image)
+    cv2.imshow('fgmask',fgmask)
+    cv2.imshow('fgmask colored',rgb_masked_image)
 
 
-   # cv2.imshow('edges_per_area_image', edges_per_area_image)
+    #cv2.imshow('edges_per_area_image', edges_per_area_image)
    # print(x1)
     if draw:
         cv2.rectangle(edged_fgbmask, (x1, y1), (x2, y2), 255, 3)
 
 
-   # cv2.imshow('edged_fgbmask',edged_fgbmask)
-    #cv2.imshow('fgmask',skin_ycrcb)
+    cv2.imshow('edged_fgbmask',edged_fgbmask)
+    cv2.imshow('fgmask',skin_ycrcb)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
