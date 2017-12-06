8ac21c11187b81872eb401be7e40e3d87b455150
AhmedSa-mir <asamir97@hotmail.com>
2017-12-06 22:11:35 +0200

    merged multi face detection with main program

--- bgsub.py
+++ bgsub.py
@@ -25,6 +25,8 @@ b = 0
 c = 0
 d = 0
 
+face_cascade = cv2.CascadeClassifier('front_face.xml')
+
 frames_count = 0;
 frame_max_x = 0;
 frame_max_y = 0;
@@ -36,222 +38,151 @@ bottomDown = (0, 0)
 
 
 ###########################################################################################################################################
+def validateBoxes(faces,boxes,centroids):
+   
+    for face in faces:
+        centroids[(int((face[0] + face[0] + face[2])/2),
+                   int((face[1] + face[1] + face[3])/2))] = -17 #reset faces counter 
+    for indx,box in enumerate(boxes):
+        counter = centroids[(int((box[0] + box[0] + box[2])/2),int((box[1] + box[1] + box[3])/2))]
+        counter += 1
+        if(counter == 7):
+            try:
+                newBoxes = np.delete(boxes,indx,axis=0)   
+            except:
+                print(indx,boxes,"!!!!!!!!!!!!")
+            boxes = newBoxes
+            counter = 0
+        centroids[(int((box[0] + box[0] + box[2])/2),int((box[1] + box[1] + box[3])/2))] = counter
+
+    return faces,boxes,centroids
+
+def detector(sendQueue,receiveQueue):
+
+    print("Detector starting...") 
+    while True:
+        if(receiveQueue.empty() == False):
+            msg = receiveQueue.get()
+            _frame = msg[0]
+            detectSingleFace(_frame,sendQueue)
+    print("Detector exiting...")
 
 
-
-def detector(sendQueue, receiveQueue, _frame, faces, exitFlag, facesNumber):
-    # print("Detector starting...")
+def detectSingleFace(_frame,sendQueue):
+    #print(_frame)
+    faces=([])
     try:
-        _frame, currentFaces, exitFlag, currentFacesNumber = detectSingleFace(_frame, faces, exitFlag, facesNumber,
-                                                                              sendQueue)
+        faces = face_cascade.detectMultiScale(_frame, 1.3, 5) # haar cascade face detection
     except:
-        print("caught it boss!")
-    currentFacesNumber = 0
-    currentFaces = np.zeros([1, 4])
+        print("error caught")
+    if len(faces) != 0:
+        print("Detected new faces...")
+        sendQueue.put([faces])
 
+
+def tracker(receiveQueue,sendQueue,mainQueue):
+   
+    print("Tracker starting...")
+    boxes = ([])
+    faces = ([])
+    centroids = np.zeros([1000, 1000])  #array of counters to remove still invalid boxes
     while True:
-        while (receiveQueue.empty() == False):
-            # print("Detector received message ")
+        if(receiveQueue.empty() == False):
             msg = receiveQueue.get()
-            _frame, temp1, exitFlag, temp2 = msg
-            _frame, currentFaces, exitFlag, currentFacesNumber = detectSingleFace(_frame, currentFaces, exitFlag,
-                                                                                  currentFacesNumber, sendQueue)
-            if exitFlag == 1:
-                break
-        if exitFlag == 1:
-            break
+            faces = msg[0]
+        faces, boxes = trackFaces(faces, boxes, receiveQueue, sendQueue, mainQueue)
+        faces, boxes, centroids = validateBoxes(faces, boxes, centroids)   #remove still invalid boxes
 
-            # print("Detector exiting...")
+    print("Tracker exiting...")
 
 
-def tracker(receiveQueue, sendQueue, mainQueue, _frame, faces, exitFlag, facesNumber):
-    # print("Tracker starting...")
-    boxes = np.zeros([1, 4])
+
+
+
+def trackFaces(faces,boxes,receiveQueue,sendQueue,mainQueue):
+
+    boxes = np.array(boxes)
+
+    #refreshing the boxes by tracking them
+    #before comparing them with the faces captured by detector
+    refresh = True
+
+    # Read frame
     ok, _frame = cap.read()
-    currentFrame = _frame
+    _frame = cv2.flip(_frame, 1)   #flip image to adjust it
 
-    while True:
-        while (receiveQueue.empty() == False):
-            # print("Tracker received message ")
-            msg = receiveQueue.get()
-            temp, faces, exitFlag, facesNumber = msg
-            if exitFlag == 1:
-                break
-            currentFrame, faces, exitFlag, facesNumber, boxes = trackFaces(currentFrame, faces, exitFlag, facesNumber,
-                                                                           boxes, receiveQueue, sendQueue, mainQueue)
-        if exitFlag == 1:
-            break
-        currentFrame, faces, exitFlag, facesNumber, boxes = trackFaces(currentFrame, faces, exitFlag, facesNumber,
-                                                                       boxes, receiveQueue, sendQueue, mainQueue)
-        # print("Tracker exiting...")
+    # Create tracker
+    tracker = cv2.MultiTracker_create()
 
+    for box in boxes:
+        ok = tracker.add(cv2.TrackerKCF_create(), _frame, (int(box[0]),int(box[1]),int(box[2]),int(box[3])))
 
-def detectSingleFace(_frame, faces, exitFlag, facesNumber, sendQueue):
-    # Display result
-    # cv2.imshow("Detector", _frame)
+    while (True):
 
-    faces = face_cascade.detectMultiScale(_frame, 1.3, 5)  # haar cascade face detection
-    if len(faces) != facesNumber:
-        # print("Detected new faces...")
-        facesNumber = len(faces)
-        sendQueue.put([_frame, faces, exitFlag, facesNumber])
+        print(boxes)
 
-    # # Exit if ESC pressed
-    # k = cv2.waitKey(30) & 0xff
-    # if k == 27:
-    #    exitFlag = 1
-    #    sendQueue.put([_frame ,faces ,exitFlag,facesNumber])
+        # Update tracker
+        ret, boxes = tracker.update(_frame)
 
-    return _frame, faces, exitFlag, facesNumber
-
-
-def trackFaces(currentFrame, faces, exitFlag, facesNumber, boxes, receiveQueue, sendQueue, mainQueue):
-    boxesA = np.copy(boxes)
-    while (facesNumber != 0 and exitFlag == 0):
-        # Create tracker
-        tracker = cv2.MultiTracker_create()
-        for face in faces:
-            ok = tracker.add(cv2.TrackerKCF_create(), currentFrame,
-                             (int(face[0]), int(face[1]), int(face[2]), int(face[3])))
-
-        # time = cv2.getTickCount()
-        while (exitFlag == 0):
-
-            # print("Tracking...")
-
-            # Start timer
-            timer = cv2.getTickCount()
-
-            # Read frame
-            ok, currentFrame = cap.read()
-            currentFrame = cv2.flip(currentFrame, 1)  # flip image to adjust it
-            # Update tracker
-            ret, boxes = tracker.update(currentFrame)
-
-            # Calculate Frames per second (FPS)
-            fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
-
-            if ret:  # Tracking success
-                boxesA = np.copy(boxes)
-                # print("tracked succesfully")
-            else:
-                break
-            if len(boxesA) == 0:
-                print("zero size!!")
-
-            for box in boxesA:
+        if ret:  # Tracking success
+            for box in boxes:
                 topLeft = (int(box[0]), int(box[1]))
-                # TODO://remove this!
-                boxA = box[2]
-                boxB = box[3]
                 bottomDown = (int(box[0] + box[2]), int(box[1] + box[3]))
-                currentFrame[topLeft[1]:bottomDown[1], topLeft[0]:bottomDown[0]] = [0, 0, 0];
-                # cv2.rectangle(currentFrame, topLeft, bottomDown, (255,0,0), 2, 1)  # Draw box
-
-            # Display tracker type on frame
-            # cv2.putText(currentFrame,"KCF Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
-
-            # Display FPS on frame
-            # cv2.putText(currentFrame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-
-            # Display result
-            # cv2.imshow("Tracking", currentFrame)
-
-
-
-            if (sendQueue.empty() == True):  # if detector did not consume the frame continue
-                sendQueue.put([currentFrame, faces, exitFlag, facesNumber])
-
+                cv2.rectangle(_frame, topLeft, bottomDown, (0,0,0),-1)  # Draw box
+            
             # Check if main program did not take the last frame update the frame
             try:
                 mainQueue.get_nowait()
-                mainQueue.put([currentFrame])
+                mainQueue.put([_frame])
             except:
-                mainQueue.put([currentFrame])
+                mainQueue.put([_frame])
+        else:
+            # Check if main program did not take the last frame update the frame
+            print("Tracking failed")
+            try:
+                mainQueue.get_nowait()
+                mainQueue.put([_frame])
+            except:
+                mainQueue.put([_frame])
 
-            # Exit if ESC pressed
-            # k = cv2.waitKey(1) & 0xff
-            # if k == 27:
-            #   exitFlag = 1
-            #   sendQueue.put([currentFrame ,faces ,exitFlag,facesNumber])
-            #   break
-
-            if (receiveQueue.empty() == 0):  # new face
-                break
-
-        # print("Exited Tracking...")
-        # Read frame
-        ok, currentFrame = cap.read()
-        currentFrame = cv2.flip(currentFrame, 1)  # flip image to adjust it
-        if (sendQueue.empty() == True):
-            sendQueue.put([currentFrame, faces, exitFlag, facesNumber])
-
-        # Exit if ESC pressed
-        # k = cv2.waitKey(1) & 0xff
-        # if k == 27:
-        #   exitFlag = 1
-        #   sendQueue.put([currentFrame ,faces ,exitFlag,facesNumber])
-        #   break
-
-        for box in boxesA:
-            topLeft = (int(box[0]), int(box[1]))
-            # TODO://remove this!
-            boxA = box[2]
-            boxB = box[3]
-            bottomDown = (int(box[0] + box[2]), int(box[1] + box[3]))
-            currentFrame[topLeft[1]:bottomDown[1], topLeft[0]:bottomDown[0]] = [0, 0, 0];
-
-        # Check if main program did not take the last frame update the frame
-        try:
-            mainQueue.get_nowait()
-            mainQueue.put([currentFrame])
-        except:
-            mainQueue.put([currentFrame])
-
-        # Display result
-        # cv2.imshow("Tracking", currentFrame)
-
-        if (receiveQueue.empty() == 0):  # new face
+            boxes = ([])
             break
 
-    # Read frame
+        if(refresh):
+            print("refreshed")
+            # Create tracker            
+            tracker = cv2.MultiTracker_create()
 
-    ok, currentFrame = cap.read()
-    # print("camera frame :")
-    # print(ok)
-    currentFrame = cv2.flip(currentFrame, 1)  # flip image to adjust it
-    tempFrame = currentFrame
-    if (sendQueue.empty() == True):
-        sendQueue.put([currentFrame, faces, exitFlag, facesNumber])
+            for face in faces:
+                center = int((face[0]+(face[0] + face[2]))/2),int((face[1]+(face[1]+face[3]))/2)
+                new = True
+                for box in boxes:
+                    if(center[0] > int(box[0]) and center[0] < int(box[0]+box[2])):
+                        if(center[1] > int(box[1]) and center[1] < int(box[1]+box[3])):
+                            new = False
+                            tempBox = boxes[boxes == box]    #update box with new face position
+                            tempBox[0:4] = np.copy(face)
+                            boxes[boxes == box] = tempBox
+                            break
+                if new:
+                    print("New face added :" ,face)
+                    ok = tracker.add(cv2.TrackerKCF_create(), _frame, (int(face[0]),int(face[1]),int(face[2]),int(face[3])))
 
-    # Exit if ESC pressed
-    # k = cv2.waitKey(1) & 0xff
-    # if k == 27:
-    #   exitFlag = 1
-    #   sendQueue.put([currentFrame ,faces ,exitFlag,facesNumber])
+            for box in boxes:
+                ok = tracker.add(cv2.TrackerKCF_create(), _frame, (int(box[0]),int(box[1]),int(box[2]),int(box[3])))
 
+            refresh = False
 
+        # Read frame
+        ok, _frame = cap.read()
+        _frame = cv2.flip(_frame, 1)   #flip image to adjust it
+        if(sendQueue.empty() == True):   #if detector did not consume the frame continue
+            sendQueue.put([_frame])
 
-    for box in boxesA:
-        topLeft = (int(box[0]), int(box[1]))
-        # TODO://remove this!
-        boxA = box[2]
-        boxB = box[3]
-        bottomDown = (int(box[0] + box[2]), int(box[1] + box[3]))
-        currentFrame[topLeft[1]:bottomDown[1], topLeft[0]:bottomDown[0]] = [0, 0, 0];
+        if(receiveQueue.empty() == 0):  # new face
+            break
 
-    # Check if main program did not take the last frame update the frame
-    try:
-        mainQueue.get_nowait()
-        mainQueue.put([currentFrame])
-    except:
-        mainQueue.put([currentFrame])
-
-    # Display result
-    # cv2.imshow("Tracking", currentFrame)
-    # print("SDGSDGDS")
-    return tempFrame, faces, exitFlag, facesNumber, boxesA
-
+    return faces,boxes
 
 ##################################################################################################################
 
@@ -319,7 +250,7 @@ call_time = 0.5
 faceExist = False;  # to figure out wether at least face exist or no
 faceWasDetectedBefore = False;
 
-face_cascade = cv2.CascadeClassifier('front_face.xml')
+
 
 
 def hsv_histogram_for_window(frame, window):
@@ -487,10 +418,10 @@ count = 0;
 q1 = Queue()
 q2 = Queue()
 frameQueue = Queue()
-frame = np.zeros([64, 480])
 # Create new processes
-facesDetector = Process(target=detector, args=(q1, q2, frame, np.zeros([1, 4]), 0, 0))
-facesTracker = Process(target=tracker, args=(q1, q2, frameQueue, frame, np.zeros([1, 4]), 0, 0))
+
+facesDetector = Process(target=detector, args=(q1, q2))
+facesTracker = Process(target=tracker, args=(q1, q2, frameQueue))
 
 # Start new processes
 facesDetector.start()
@@ -776,6 +707,8 @@ while (1):
         # reset counter if 'r' is pressed //debugging mode
         # frame_counter_temp=0
     if k == 27:
+        facesDetector.terminate()
+        facesTracker.terminate()
         break
 
 end_time = datetime.now()
